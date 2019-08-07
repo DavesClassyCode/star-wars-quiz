5f0ff3815c90208b3624250703fb80ab6c456cf7
David Nowosad <davidrnowosad@gmail.com>
2019-08-07 07:15:21 -0400

    added questions and answers to datastore.js

---
+++ datastore.js
@@ -0,0 +1,102 @@
+const STORE = [
+    {
+      question: 'Which Star Wars movie was filmed entirely in the studio?',
+      answers: [
+        'Revenge of the Sith',
+        'Attack of the Clones',
+        'Return of the Jedi',
+        'Star Wars'
+        ],
+      correctAnswer: 'Revenge of the Sith'
+    },
+    {
+      question: 'Which of the three main heroes (Luke, Leia, and Han Solo) in the first Star Wars trilogy refused to sign a three picture deal?',
+      answers: [
+        'Harrison Ford',
+        'Mark Hamill',
+        'Carrie Fisher',
+        'They All Signed'
+        ],
+        correctAnswer: 'Harrison Ford'
+    },
+    {
+      question: 'What color is Mace Windu\'s lightsaber?',
+      answers: [
+        'Green',
+        'Purple',
+        'Black',
+        'Blue'
+        ],
+        correctAnswer: 'Purple'
+    },
+    {
+      question: '"The Star Wars Holiday Special" marked the first appearance of which Star Wars character?',
+      answers: [
+        'Jabba the Hutt',
+        'Boba Fett',
+        'Jar Jar Binks',
+        'Lando Calrissian'
+        ],
+      correctAnswer: 'Boba Fett'
+    },
+    {
+      question: 'What actor pulled out of Episode III: Revenge of the Sith when he discovered that non-union actors were being used in the film?',
+      answers: [
+        'Mel Gibson',
+        'Gary Oldman',
+        'George Clooney',
+        'Gary Coleman'
+        ],
+      correctAnswer: 'Gary Oldman'
+    },
+    {
+      question: 'Who is the only non Jedi in the original Star Wars trilogy to use a lightsaber?',
+      answers: [
+        'Chewbacca',
+        'R2-D2',
+        'Han Solo',
+        'Princess Leia'
+        ],
+      correctAnswer: 'Han Solo'
+    },
+    {
+      question: 'What character did George Lucas consider making a midget?',
+      answers: [
+      'Princess Leia',
+      'Jar Jar Binks',
+      'Luke Skywalker',
+      'Chewbacca'
+      ],
+      correctAnswer: 'Luke Skywalker'
+    },
+    {
+      question: 'Who served as Jabba the Hutt\'s chief of staff?',
+      answers: [
+        'Sarlacc',
+        'Nute Gunray',
+        'Maz Kanata',
+        'Bib Fortuna'
+      ],
+      correctAnswer: 'Bib Fortuna'
+    },
+    {
+      question: 'Approximately how many languages can C-3PO speak?',
+      answers: [
+      '6 Trillion',
+      '6 Million',
+      '6 Thousand',
+      '6 Billion'
+      ],
+      correctAnswer: '6 Million'
+    },
+    {
+      question: 'In Episode VI: Return of the Jedi, the growls and sounds of the Rancor in Jabba\'s Palace were actually made by what animal?',
+      answers: [
+        'Panther',
+        'Wolverine',
+        'Dachsund',
+        'Pigeon'
+    ],
+    correctAnswer: 'Panther'
+    }
+];
