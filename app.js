c2a1171de3cd2c850b01a93dcb526580520644b2
DavesClassyCode <davidrnowosad@gmail.com>
2022-08-24 12:03:27 -0400

    change `.questionNumber` to `.js-questionNumber`

--- app.js
+++ app.js
@@ -34,14 +34,14 @@ function generateQuestion() {
 } else {
     renderResults();
     restartQuiz();
-    $('.questionNumber').text(10)
+    $('.js-questionNumber').text(10)
   }
 }
 
 //increment question number
 function nextQuestion() {
     questionNumber ++;
-  $('.questionNumber').text(questionNumber+1);
+  $('.js-questionNumber').text(questionNumber+1);
 }
 
 //increment score
@@ -56,7 +56,7 @@ function startQuiz() {
   $('.quizStart').on('click', '.startButton', function (event) {
     $('.quizStart').remove();
     $('.questionAnswerForm').css('display', 'block');
-    $('.questionNumber').text(1);
+    $('.js-questionNumber').text(1);
 });
 }
 
